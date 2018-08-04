4cf08effed610c9121fe50db89f9284c1316ed8e
Sigbjorn Helset <sigbjorn.helset@gmail.com>
2018-08-04 06:36:18 +0200

    updated netcdftime to cftime

--- shyft/tests/test_netcdftime.py
+++ shyft/tests/test_netcdftime.py
@@ -1,7 +1,7 @@
 import unittest
 from shyft import api
 from shyft.repository.netcdf.time_conversion import convert_netcdf_time
-from netcdftime import utime
+from cftime import utime
 import numpy as np
 
 
