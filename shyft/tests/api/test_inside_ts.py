ad51e5f9b096609e4c815d3db90a7424364b448f
Tobias <tobias.litherland@statkraft.com>
2018-10-05 14:14:14 +0200

    Added test for issue #352 (#353)

--- shyft/tests/api/test_inside_ts.py
+++ shyft/tests/api/test_inside_ts.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 import unittest
-from shyft.api import TimeSeries, Calendar, TimeAxis, deltahours, DoubleVector, POINT_AVERAGE_VALUE
+from shyft.api import TimeSeries, Calendar, TimeAxis, deltahours, DoubleVector, POINT_AVERAGE_VALUE, derivative_method
 
 
 class InsideTimeSeries(unittest.TestCase):
@@ -27,3 +27,22 @@ class InsideTimeSeries(unittest.TestCase):
         ts = TimeSeries(TimeAxis(t0, dt, len(dv)), dv, POINT_AVERAGE_VALUE)
         i2 = ts.inside(min_v=2.0, max_v=3.0, nan_v=1.0, inside_v=0.0, outside_v=1.0)
         assert_array_almost_equal(i2.values.to_numpy(), np.array(i1_ex))
+
+    def test_inside_of_derivative(self):
+        """Created in response to https://github.com/statkraft/shyft/issues/352"""
+        values = [1, 1, 1, 1]
+        utc = Calendar()
+        data = np.array(values, dtype='float64')
+        data_ta = TimeAxis(utc.time(2015, 1, 1), 3600, len(data))
+
+        orig = TimeSeries(data_ta, data, point_interpretation_policy.POINT_AVERAGE_VALUE)
+        orig_derivative_inside_inf = orig.derivative(derivative_method.BACKWARD).inside(-float('inf'), float('inf'), 0,
+                                                                                        1, 0)
+
+        def check_nan(ts):
+            """Method that returns 1 for all timesteps that contain nan."""
+            return ts.inside(-float('inf'), float('inf'), 1, 0, 0).values.to_numpy()
+
+        np.testing.assert_equal(check_nan(orig + orig_derivative_inside_inf), [0., 0., 0., 0.],
+                                'TimeSeries.inside() should not find any NaN-values in this TimeSeries')
+
