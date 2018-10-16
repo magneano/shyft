466b379a06073cafb6a90fa3cc8c8d4f4a4c514d
osilan <olga.silantyeva@geo.uio.no>
2018-10-16 14:22:40 +0200

    added instantaneous extraterrestrial radiation, first steps

--- test/radiation_test.cpp
+++ test/radiation_test.cpp
@@ -3,22 +3,58 @@
 #include "core/geo_cell_data.h"
 #include <armadillo>
 #include <vector>
+#include <boost/math/constants/constants.hpp>
 
 namespace shyft::core {
 using std::vector;
-
+using std::cos;
+using std::sin;
+const double pi = boost::math::constants::pi<double>();
 
 struct radiation_calculator {
     calendar utc;
+    double doy;
+    double hour;
+    double s = 0.0; // horizontal slope
+    double delta = 0.1; // declination of the earth, positive for northern hemisphere summer
+    double gamma = 0.0; // surface aspect angle, 0 -- due south, -pi/2 -- due east, pi/2 -- due west, +-pi -- due north
+    double phi = 0.0 ;// latitude
+    double ra = 0; // extraterrestrial solar radiation [W/m2]
+    double rso = 0; // clear sky solar radiation [W/m2]
     
     /** \tparam V is typically arma::vec */
     template <class V>
     double real_radiation(double latitude, utctime t, const V & surface_normal) {
-        auto doy=utc.day_of_year(t);
-        auto hour = utc.calendar_units(t).hour;
+        doy=utc.day_of_year(t);
+        hour = utc.calendar_units(t).hour;
         return hour;
     }
-    
+    const double gsc = 1367; // W/m2 -- solar constant
+    /** \brief computes instantaneous extraterrestrial solar radiation*/
+    template <class V>
+    double ra_radiation(double latitude, utctime t, const V & surface_normal){
+        phi = latitude;
+        doy = utc.day_of_year(t);
+        delta = 23.45*pi/180*sin(2*pi*(284+doy)/36.25); // declination angle based on eq.(3.1) https://www.itacanet.org/the-sun-as-a-source-of-energy/part-3-calculating-solar-angles/
+        hour = utc.calendar_units(t).hour;
+        compute_costt(hour);
+        ra = gsc*cos_tt*(1+0.0033*cos(doy*2*pi/365)); // eq.(1)
+        return ra;
+    }
+    /*double rso_cs_radiation(){
+        return ra*tau_swo;
+    }*/
+private:
+    double a, b, c, cos_tt;
+    /** \brief computes necessary geometric parameters
+     * \param omega -- hour angle
+     * */
+    void compute_costt(double omega){
+        a = sin(delta)*cos(phi)*sin(s)*cos(gamma) - sin(delta)*sin(phi)*cos(s); // eq.11a
+        b = cos(delta)*cos(phi)*cos(s) + cos(delta)*sin(phi)*sin(s)*cos(gamma);//eq 11b
+        c = cos(delta)*sin(s)*sin(gamma);
+        cos_tt = -a + b*cos(omega) + c*sin(omega); // eq.14
+    }
 };
 
 /** \tparam C is a cell, like shyft-cell, */
